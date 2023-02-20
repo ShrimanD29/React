eeeb9498c6f591e6442f1cdf4d21e6b65d89be11
ShrimanD29 <124685898+ShrimanD29@users.noreply.github.com>
2023-02-20 12:31:28 +0530

    Add files via upload

---
+++ src/Description.js
@@ -0,0 +1,8 @@
+import  './Description.css';
+function Description(){
+    return(
+        <p id="des">LAKMÉ Perfecting Liquid Foundation Pearl,<br></br>Waterproof Full Coverage Long Lasting,<br></br> Light Oil Free Face Makeup with Vitamin E,<br></br>Dewy Finish Glow, 27ml</p>
+    );
+    }
+    
+    export default Description;
